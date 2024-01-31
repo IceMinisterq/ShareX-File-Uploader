e3864bc811e1ba90330aa5498ca29db70acdfd3e
Iceminister <86623018+IceMinisterq@users.noreply.github.com>
2024-01-31 16:21:55 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
-const express              = require("express");
-const formidableMiddleware = require("express-formidable");
-const fs                   = require("fs");
-const randomstring         = require("randomstring");
-const path                 = require("path");
-const config               = require("./config.json")
+const randomstring = require("randomstring");
+const path         = require("path");
+const fileUpload   = require("express-fileupload");
+const express      = require("express");
 
-const UPLOAD_PATH      = config["Upload Path"]
-const BASE_URL         = config["Base URL"]
-const AUTHORIZATION    = config.Authorization
-const FILE_NAME_LENGHT = config["File Name Lenght"]
-const PORT             = config.Port
+const config       = require("./config.json");
+
+const UPLOAD_PATH      = config["Upload Path"];
+const BASE_URL         = config["Base URL"];
+const AUTHORIZATION    = config.Authorization;
+const FILE_NAME_LENGHT = config["File Name Lenght"];
+const PORT             = config.Port;
 
 
-const server = express();
+const app = express();
 
-server.use(formidableMiddleware());
+app.use(fileUpload());
 
-server.post("/upload", (req, res) => {
-  const field = req.fields
+app.post("/upload", (req, res) => {
+  const field = req.body || {}
 
-  const authKey = field.authorization || "NONE";
+  const authKey = field["authorization"] || "NONE";
 
-  if (!authKey || authKey != AUTHORIZATION) {
-    return res.status(401).end("Not authorized");
+  if (! authKey || authKey != AUTHORIZATION) {
+    return res.status(401).end("Not authorized 1");
   }
 
-  if (!req.files || typeof(req.files) != "object") {
-    return res.status(401).end("Not authorized.");
+  if (! req.files || typeof(req.files) != "object") {
+    return res.status(401).end("Not authorized 2"); // I honestly don't even know why i even made this check in the first place
   }
 
   const files = Object.values(req.files);
@@ -38,7 +38,6 @@ server.post("/upload", (req, res) => {
   }
 
   const file       = files[0];
-  const filePath   = file.path;
   const fileFormat = path.extname(file.name).toLowerCase();
 
   const generatedName = randomstring.generate({
@@ -48,20 +47,25 @@ server.post("/upload", (req, res) => {
 
   const fileName = generatedName + fileFormat;
 
+  // console.log(generatedName + fileFormat, fileName)
+
   const destinationPath = `${UPLOAD_PATH}${fileName}`;
   const urlDestination  = `${BASE_URL}${fileName}`;
 
-  fs.copyFile(filePath, destinationPath, (err) => {
+  // console.log(destinationPath)
+  
+  file.mv(destinationPath, function(err){
     if (err) {
-      console.log(err);
-      return res.status(500).end(`Error while saving file ${fileName}.`);
+      console.log(`err : ${err}`);
+      return res.status(500).end(`Error while saving file ${file.name}.`);
     }
   });
 
   return res.status(202).end(urlDestination);
 })
 
-server.listen(PORT, () => {
+
+app.listen(PORT, () => {
   console.log(`Share X file host is running on port ${PORT}`);
 
   if (AUTHORIZATION === "YOUR_AUTORIZATION") {
